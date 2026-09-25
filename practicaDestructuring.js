cfb0c8cbb02871f6fbe961db6c99de6eec224d17
Tavo <gacadenamz@gmail.com>
2026-09-24 20:58:28 -0500

    Iniciando la practica

---
+++ practicaDestructuring.js
@@ -0,0 +1,29 @@
+/* 
+1. Usa desestructuración para extraer los dos primeros elementos de un array 
+2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
+3. Usa desestructuración para extraer dos propiedades de un objeto
+4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas a nuevas variables con nombres diferentes
+5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+6. Usa propagación para combinar dos arrays en uno nuevo
+7. Usa propagación para crear una copia de un array
+8. Usa propagación para combinar dos objetos en uno nuevo
+9. Usa propagación para crear una copia de un objeto
+10. Combina desestructuración y propagación para extraer una propiedad de un objeto y crear un nuevo objeto sin esa propiedad
+11. Combina desestructuración y propagación para extraer un elemento de un array y crear un nuevo array sin ese elemento
+*/
+
+let nombres = ["Juan", "Luis", "Sara", "Paz"];
+
+let[nom1, nom2] = nombres;
+
+console.log(nom1, nom2);
+
+let numeros = [
+    [2, 4, 6, 8],
+    [1, 3, 5, 7]
+]
+
+let[pares, impares] = numeros;
+
+console.log(pares);
+
